7c98bd9625158f6b63c156cede61f523b8561cd0
Ismaellamgadar <90750867+Ismaellamgadar@users.noreply.github.com>
2021-10-19 08:28:29 +0200

    first push

---
+++ Durschnittsnote/src/Durchschnittsnote.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Durchschnittsnote {
+
+    public static void main(String[] args) {
+
+        int erstenotee;
+        int zweitenote;
+        double addiert;
+        int n;
+        float[] num = new float[100];
+        float sum = 0.0f, avg;
+
+        System.out.print("Geben sie ihre Anzahl Noten an: ");
+        n = STDIN_SCANNER.nextInt();
+
+        while(n > 10 || n < 1) {                    //
+            System.out.println("Error! Nummer muss zwischen 1 und 10 sein");
+            System.out.print("Geben sie ihre Anzahl noch mal an: ");
+            n = STDIN_SCANNER.nextInt();
+        }
+        for(int i = 0; i < n; ++i) {
+            System.out.print((i + 1) + ". Note: ");
+            num[i] = STDIN_SCANNER.nextFloat();
+            sum += num[i];
+        }
+        avg = sum / n;
+        System.out.printf("\nDurchschnitt = %.2f \n\n\n", avg);
+    }
+
+    public final static Scanner STDIN_SCANNER = new Scanner(System.in);
+}

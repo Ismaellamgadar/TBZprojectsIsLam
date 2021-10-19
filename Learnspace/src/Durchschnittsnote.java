0f4500026946a462f7046a923cc38a340c38d32d
Ismaellamgadar <90750867+Ismaellamgadar@users.noreply.github.com>
2021-10-19 08:55:48 +0200

    Created Learnspace

---
+++ Learnspace/src/Durchschnittsnote.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Durchschnittsnote {
+
+    public static void main(String[] args) {
+
+
+        int n;    //Anzahl Noten
+        float[] num = new float[100];
+        float sum = 0.0f, avg;
+
+        System.out.print("Geben sie ihre Anzahl Noten an: ");
+        n = STDIN_SCANNER.nextInt();        //Scannt Eingabe dh. Anzahl Noten
+
+        while(n > 10 || n < 1) {                                                // Der Benutzer kann minimal 1 und maximal 10 Noten angeben.
+            System.out.println("Error! Nummer muss zwischen 1 und 10 sein");    // Text falls dies nicht respektiert wird.
+            System.out.print("Geben sie ihre Anzahl noch mal an: ");
+            n = STDIN_SCANNER.nextInt();
+        }
+        for(int i = 0; i < n; ++i) {        // solang i kleiner als die Anzahl angegebenen Noten ist, wird diese Schlaufe wiederholt.
+            System.out.print((i + 1) + ". Note: ");
+            num[i] = STDIN_SCANNER.nextFloat();
+            sum += num[i];
+        }
+        avg = sum / n;                      // avg ist der Durchschnitt(Average)
+        System.out.printf("\nDurchschnitt = %.2f \n\n\n", avg);
+    }
+
+    public final static Scanner STDIN_SCANNER = new Scanner(System.in);
+}

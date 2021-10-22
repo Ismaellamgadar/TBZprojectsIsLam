cf2c42ef0449086d4a4088f5c48572c1f7063e99
Ismaellamgadar <90750867+Ismaellamgadar@users.noreply.github.com>
2021-10-22 11:27:42 +0200

    test

---
+++ Java/Learnspace/src/org/launchcode/Main.java
@@ -0,0 +1,20 @@
+package org.launchcode;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+        public static void main(String[] args) {
+
+            char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                            {'-', '|', '-', '|', '-'},
+                            {'-', '|', '-', '|', '-'},
+                            {'-', '|', '-', '|', '-'},
+                            {' ', '|', ' ', '|', ' '}};
+
+            System.out.print(gameBoard);
+        }
+
+}
